33f54be8b14ae38b8503f9545b9abc1eaf04eb7a
Kholod13 <vlad13holod@gmail.com>
2025-06-24 14:04:04 +0200

    fix folders design

--- components/Folders.js
+++ components/Folders.js
@@ -1,4 +1,4 @@
-import { StyleSheet, Text, View, Image, TouchableOpacity, ScrollView, Modal } from 'react-native';
+import { StyleSheet, Text, View, Image, TouchableOpacity, ScrollView, Modal, TextInput } from 'react-native';
 import { GenStyles } from '../styles/style';
 import { useNavigation } from '@react-navigation/native';
 import NavigationMenu from './NavigationMenu';
@@ -7,16 +7,24 @@ import React, { useState } from 'react';
 
 export default function Folders() {
     const navigation = useNavigation();
-    const { usersList, currentUserEmail } = useUsers();
+    const { usersList, currentUserEmail, addFolderToUser } = useUsers();
     const currentUser = usersList.find(user => user.email === currentUserEmail);
     const folders = currentUser?.folders || [];
     const [modalVisible, setModalVisible] = useState(false);
+    const [modalVisible1, setModalVisible1] = useState(false);
     const [selectedFolder, setSelectedFolder] = useState(null);
+    const [newFolderName, setNewFolderName] = useState('');
 
   return (
     <View style={GenStyles.containerLoggedIn}>
         <View style={GenStyles.containerContent}>
-            <Text style={GenStyles.title}>FOLDER CONTAINER</Text>
+            <View style={[GenStyles.containerHeader, {backgroundColor: '#fff', marginTop: -20, marginBottom: 30, alignItems: 'center', paddingTop: 45, paddingBottom: 15, paddingHorizontal: 10}]}>
+                      <View style={{width: 20}}></View>
+                      <Text style={[GenStyles.text, {fontFamily: 'inter-bold', fontSize: 20}]}>My folders</Text>
+                      <TouchableOpacity onPress={() => setModalVisible1(true)}>
+                        <Image source={require('../assets/icons/Plus.png')} style={{width: 25, height: 25, marginTop: -15}}/>
+                      </TouchableOpacity>
+                  </View>
               <Modal visible={modalVisible} animationType="slide">
                 <View style={{ flex: 1, alignItems: 'center', marginTop: 30 }}>
                   <View style={GenStyles.containerHeader}>
@@ -41,7 +49,59 @@ export default function Folders() {
                   </ScrollView>
                 </View>
               </Modal>
-              <ScrollView showsVerticalScrollIndicator={false} style={{ marginTop: -20, height: '65%' }}>
+              <Modal visible={modalVisible1} transparent animationType="slide">
+                <View style={{
+                  flex: 1,
+                  justifyContent: 'center',
+                  alignItems: 'center',
+                  backgroundColor: 'rgba(0,0,0,0.5)'
+                }}>
+                  <View style={{
+                    backgroundColor: '#fff',
+                    padding: 20,
+                    borderRadius: 12,
+                    width: '100%',
+                    height: '100%',
+                    alignItems: 'center',
+                    paddingTop: 50,
+                  }}>
+                    <View style={GenStyles.containerHeader}>
+                      <TouchableOpacity onPress={() => setModalVisible(false)} style={{ marginRight: 10 }}>
+                        <Image source={require('../assets/icons/ArrowLeft.png')} />
+                      </TouchableOpacity>
+                      <Text style={{ fontFamily: 'inter-bold', fontSize: 18 }}>New Folder</Text>
+                      <TouchableOpacity
+                        onPress={() => {
+                          const newId = `f${Date.now()}`; // генерируем уникальный ID
+                          const newFolder = { id: newId, name: newFolderName, words: [] };
+                          addFolderToUser(currentUser.email, newFolder);
+                          // Сначала закроем модалку и сбросим состояние
+                          setModalVisible1(false);
+                          setNewFolderName('');
+                          // Перейдём на главный экран
+                          navigation.navigate('Folders');
+                        }}
+                      >
+                        <Text style={{ fontWeight: 'bold', fontSize: 18, color: '#0388F5' }}>Save</Text>
+                      </TouchableOpacity>
+                    </View>
+                    <View style={{width: '100%', flex: 1}}>
+                      <Text style={[styles.underText, {marginTop: 30, textAlign: 'left'}]}>Folder name</Text>
+                      <TextInput
+                        placeholder="Folder name"
+                        value={newFolderName}
+                        onChangeText={setNewFolderName}
+                        style={styles.input}
+                      />
+                      </View>
+                  </View>
+                </View>
+              </Modal>
+              <View style={{flexDirection: 'row', alignItems: 'center'}}>
+                <Image source={require('../assets/icons/USA.png')} style={{width: 15, height: 15, marginTop: -30, marginLeft: 20}} />
+                <Text style={[GenStyles.underText, {marginBottom: 30, color: '#918D8A', marginLeft: 10}]}>English</Text>
+              </View>
+              <ScrollView showsVerticalScrollIndicator={false} style={{ marginTop: -20, height: '65%', paddingHorizontal: 20 }}>
                 {folders.length > 0 ? (
                   folders.map((folder, index) => (
                     <TouchableOpacity key={index} 
@@ -69,6 +129,7 @@ export default function Folders() {
                         </Text>
                       )}
               </ScrollView>
+              {/* hidden menu "Archive" */}
         </View>
         <NavigationMenu />
     </View>
